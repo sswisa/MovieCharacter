9f2e7099ce790c3d695e27dfcb57261d272becee
shimon swisa <sswisadevs@gmail.com>
2019-06-25 14:55:16 -0400

    initial heroku deploy'

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
-var express = require('express');
-var path = require('path');
+const path = require('path');
+const express = require('express');
+const app = express();
 
-var app = express();
+// Serve static files
+app.use(express.static(__dirname + '/dist/movie-character'));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/movie-character/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
